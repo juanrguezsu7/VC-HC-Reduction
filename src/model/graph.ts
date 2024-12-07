3306b4e5eadb14a5075cf2eb44796db3ae7e03fb
Juan Rodríguez Suárez <alu0101477596@ull.edu.es>
2024-12-07 17:34:18 +0000

    Main class files

---
+++ src/model/graph.ts
@@ -0,0 +1,18 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Complejidad Computacional
+ *
+ * @since Dec 07, 2024
+ * @description Contains the definition of a generic graph.
+ * @exports Graph
+ */
+
+/**
+ * @description Graph class to represent a generic graph.
+ * @abstract
+ */
+export abstract class Graph {
+  
+}
