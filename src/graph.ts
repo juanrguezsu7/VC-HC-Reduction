043455be21db975588dfe480c80675c3b0a5e071
Juan Rodríguez Suárez <alu0101477596@ull.edu.es>
2024-12-07 17:27:34 +0000

    Graph class declaration

---
+++ src/graph.ts
@@ -0,0 +1,17 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Complejidad Computacional
+ *
+ * @since Dec 07, 2024
+ * @description Contains the definition of the Graph class to represent a graph from either a VC problem or a HC problem.
+ */
+
+/**
+ * @description Graph class to represent a graph from either a VC problem or a HC problem.
+ * @export
+ */
+export class Graph {
+  
+}
